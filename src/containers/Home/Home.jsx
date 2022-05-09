04a7e580a69df63c600bc7948fcfc75a473ebcb0
ocampott <ocampott7@gmail.com>
2022-05-08 23:36:24 -0300

    Added hr and heart to home - Fixed some styles

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -3,9 +3,10 @@ import "./Home.css";
 import { DogList } from "../../components/DogList/DogList";
 import { Filter } from "../../components/Filter/Filter";
 import { getDogsBySearch } from "../../services/getDogsBySearch";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
 export const Home = () => {
-  
   /*Guardo la data del API */
   const [dogList, setDogList] = useState([]);
   const [dogsFavoriteImg, setDogsFavoriteImg] = useState([]);
@@ -50,12 +51,25 @@ export const Home = () => {
             Escriba una raza de perro en el buscador para ver hasta 10 imagenes
             de la misma.
           </p>
-          <p>Las razas se pueden encontrar en este <a href="https://dog.ceo/dog-api/documentation/">link</a></p>
+          <p>
+            Las razas se pueden encontrar en este{" "}
+            <a href="https://dog.ceo/dog-api/documentation/">link</a>
+          </p>
         </div>
       ) : (
         <DogList dogList={dogList} setFavorites={setFavorites} />
       )}
-      {dogsFavoriteImg.length === 0 ? <></> : <h1 className="home-h1">Favoritos</h1>}
+      {dogsFavoriteImg.length === 0 ? (
+        <></>
+      ) : (
+        <>
+        <hr className="home-hr"/>
+          <h1 className="home-h1">
+            <FontAwesomeIcon className="home-heart" icon={faHeart} />
+            Favoritos
+          </h1>
+        </>
+      )}
       <DogList dogList={dogsFavoriteImg} setFavorites={setFavorites} />
     </div>
   );
